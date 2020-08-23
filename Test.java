3cfa6f58f3b57a81ccaf365e15ee860eba134e62
kanishk-seth <kanishkseth08@outlook.com>
2020-08-23 20:54:52 +0530

    Test.java Added

---
+++ Test.java
@@ -0,0 +1,42 @@
+// Java program to understand 
+// the concept of == operator 
+public class Test { 
+	/* public static void main(String[] args) 
+	{ 
+		String s1 = new String("HELLO"); 
+		String s2 = new String("HELLO");
+		String s3 = null;
+		String s4 = null;
+		int x = 9;
+		System.out.println(s1 == s2); 
+		System.out.println(s1.equals(s2)); 
+		System.out.println("HELLO".equals(s2));
+		
+		if(s1 != null && s3 !=null && Integer.parseInt(s4) >= x)
+			System.out.println("Condition is true");
+		else
+			System.out.println("Condition is not true");
+	} */ 
+	
+	/* public static void main(String[] args){
+		boolean a = checkStatus();
+		if(a)
+			System.out.println("true");
+		else
+			System.out.println("false");
+		
+	}
+	
+	public static boolean checkStatus(){
+		return false;
+	} */
+	
+	public static void main(String args[]){  
+		//declaring an array  
+		int arr[]={12,13,14,44};  
+		//traversing the array with for-each loop  
+	   for(int i:arr){  
+		 System.out.println(i);  
+	   } 
+	}   
+}
